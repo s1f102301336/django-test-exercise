75e374d5b37dc4eb48db3b1fb29866be2f7807d6
iniad <s1f102301336@iniad.org>
2024-07-04 11:39:30 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -12,6 +12,7 @@ class SampleTestCase(TestCase):
         self.assertEqual(1 + 2, 3)
 
 class TaskModelTestCase(TestCase):
+
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
